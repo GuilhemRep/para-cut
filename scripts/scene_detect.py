45a118e795b160ae9bed638667213deb1bd906f2
aalbert <augustin.albert@bleu-azure.fr>
2025-08-05 18:08:05 +0200

    change parameters of detection algorithm: still not suited

--- scripts/scene_detect.py
+++ scripts/scene_detect.py
@@ -1,6 +1,6 @@
 from scenedetect import detect, AdaptiveDetector, split_video_ffmpeg
 video_path = 'data/top_gun.mp4'
 output_path = 'data'
-scene_list = detect(video_path, AdaptiveDetector())
-print(scene_list)
+scene_list = detect(video_path, AdaptiveDetector(adaptive_threshold=1.7))
+print(len(scene_list))
 split_video_ffmpeg(video_path, scene_list, output_path)
