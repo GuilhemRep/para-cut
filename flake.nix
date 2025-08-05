47acea997eef73e44cfb9520f835585888182184
aalbert <augustin.albert@bleu-azure.fr>
2025-08-05 17:14:48 +0200

    add basic flake to manage python

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A basic Nix flake to keep track of python";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python312.withPackages (ps: with ps; [
+          numpy
+          opencv-python-headless
+        ]);
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            pkgs.ruff
+          ];
+        };
+      });
+}
